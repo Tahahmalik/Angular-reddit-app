dffb607fd3cdaf859f5ad0f66b70eb5f6d481f01
Taha Malik <taha.malik87@gmail.com>
2017-06-23 17:59:19 -0700

    Angular Reddit App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
-
+import { Article } from './article/article.model';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+articles: Article[];
+
+
+constructor() {
+	this.articles = [
+	new Article('Angular 2', 'http://angular.io', 3),
+	new Article('Fullstack', 'http://fullstck.io', 2),
+	new Article('Angular Homepage','http://angular.io', 1),
+	];
+}
+
+
 }
